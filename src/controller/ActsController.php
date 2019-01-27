3254887ec25c00f14dc32c0d0da7d5b4d7ad17ab
LiquidPirateCat <lucas.verhoest@icloud.com>
2019-01-27 18:11:05 +0100

    filter collapse mobile

---
+++ src/controller/ActsController.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/Controller.php';
+
+// require_once __DIR__ . '/../dao/SpeakerDAO.php';
+
+class ActsController extends Controller {
+
+  public function index() {
+    // $speakerDAO = new SpeakerDAO();
+    // $speakers = $speakerDAO->selectAll();
+    // $this->set('speakers', $speakers);
+    // $this->set('title', 'Home');
+    // $this->set('currentPage', 'home');
+  }
+
+
+
+}
