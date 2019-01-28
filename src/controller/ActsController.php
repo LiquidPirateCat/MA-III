4af5d46bee5d9f9eb18d2b3907fc950e201b4006
LiquidPirateCat <lucas.verhoest@icloud.com>
2019-01-28 12:14:48 +0100

    website's toevoegen aan DB

--- src/controller/ActsController.php
+++ src/controller/ActsController.php
@@ -2,16 +2,34 @@
 
 require_once __DIR__ . '/Controller.php';
 
+require_once __DIR__ . '/../dao/ActDAO.php';
+require_once __DIR__ . '/../dao/ActTagDAO.php';
+require_once __DIR__ . '/../dao/ActPortfolioItemDAO.php';
+require_once __DIR__ . '/../dao/ActDatumDAO.php';
+
+
 // require_once __DIR__ . '/../dao/SpeakerDAO.php';
 
 class ActsController extends Controller {
 
-  public function index() {
-    // $speakerDAO = new SpeakerDAO();
-    // $speakers = $speakerDAO->selectAll();
-    // $this->set('speakers', $speakers);
-    // $this->set('title', 'Home');
-    // $this->set('currentPage', 'home');
+  public function detail() {
+    $actDAO = new ActDAO();
+    $actTagDAO = new ActTagDAO();
+    $actPortfolioItemDAO = new ActPortfolioItemDAO();
+    $actDatumDAO = new ActDatumDAO();
+    if (!empty($_GET['id'])) {
+      $act = $actDAO->selectById($_GET['id']);
+    }
+    if (empty($act)) {
+      header('Location: index.php');
+      exit();
+    }
+    $this->set('act', $act);
+    $this->set('actTags', $actTagDAO->selectByActId($act['id']));
+    $this->set('actPortfolioItems', $actPortfolioItemDAO->selectByActId($act['id']));
+    $this->set('actDatums', $actDatumDAO->selectByActId($act['id']));
+    $this->set('title', $act['titel']);
+    $this->set('currentPage', 'home');
   }
 
 
--- src/view/acts/detail.php
+++ src/view/acts/detail.php
@@ -3,16 +3,16 @@
 <article class="detail-act">
 <div class="foto-container-acts detail-img" style="background-image: url(<?php echo "./" . $act['img'];?>)"></div>
 
-  <h1 class="titel gold">i-puppets</h1>
-  <h2 class="detail-groep titel black">Close-act theatre</h2>
-  <p class="detail-beschrijving txt">Ogen trekken aan je voorbij. De onderzoekers van deze planeet zijn vreemd, grappig en angstaanjagend.</p>
-  <a href="www.closeacttheater.com" class="link detail-website txt">www.closeacttheater.com</a>
+  <h1 class="titel gold"><?php echo $act['titel'];?></h1>
+  <h2 class="detail-groep titel black"><?php echo $act['artiest'];?></h2>
+  <p class="detail-beschrijving txt"><?php echo $act['bio'];?></p>
+  <a href="www.closeacttheater.com" class="link detail-website txt"><?php echo $act['website'];?></a>
   <div class="detail-praktisch">
     <div class="detail-datum titel"><span class="dag-filter">vrij</span><span class="datum-filter">24|08</span></div>
     <div class="detail-tijdsduur titel">40 <span class="dag-filter">min</span> </div>
   </div>
-  <p class="detail-locatie titel gray">Oud atletiekplein</p>
-  <p class="detail-uur titel">18u30</p>
+  <p class="detail-locatie titel gray"><?php echo $act['locatie'];?></p>
+  <p class="detail-uur titel"><?php echo $act['uur'];?>0</p>
   </article>
 
   <img src="./assets/img/robots.jpg" width="100%" alt="robots" class="detail-omslagfoto">
